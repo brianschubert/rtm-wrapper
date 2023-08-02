97cac4d7596fbf1a69e5761b38a2fec048b7e3d9
Brian Schubert <schubert.b@northeastern.edu>
2023-08-02 11:32:02 -0400

    Set import package version from distribution metadata

--- src/rtm_wrapper/__init__.py
+++ src/rtm_wrapper/__init__.py
@@ -1,3 +1,4 @@
+import importlib.metadata
+
 # Distribution version, PEP-440 compatible.
-# Should be kept in sync with 'tool.poetry.version' in pyproject.toml.
-__version__ = "0.1.0-dev"
+__version__ = importlib.metadata.version("rtm-wrapper")
