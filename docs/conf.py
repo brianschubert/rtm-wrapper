3b34a272a8555f549e7d3e055808e4b65ada51d3
Brian Schubert <schubert.b@northeastern.edu>
2023-08-07 21:24:03 -0400

    Add initial sphinx config

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import importlib.metadata
+
+import rtm_wrapper.util
+
+project = "RTM Wrapper"
+copyright = "2023, Brian Schubert"
+author = " & ".join(importlib.metadata.metadata("rtm-wrapper").get_all("Author"))
+release = rtm_wrapper.__version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx_rtd_theme"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
